a91d515732470d707da562315929162c5c9d9593
Daniel Merchan <daniel.merchan.garcia@gmail.com>
2024-05-26 21:31:45 +0200

    S_8_8_1 finalised

--- Projects/S_8_8_1/README.md
+++ Projects/S_8_8_1/README.md
@@ -41,7 +41,30 @@ Example steps:
 - (Not in the example as we are not using RGB0 rotating screen) Setup the rotating screen using `slRparaInitSet`
 - Stop drawing by invoking `slTVOff`
 - Set the color mode with `slColRam` rountine. In this case we use `CRM16_1024` (mode 0, 16 bit x 1024 color). This control the color for all palette-type sprites and scroll screens
-- Set background screen (single color) 
+- Set background screen (single color) invoking `slBack1ColSet`
+    - The first parameter indicates where in VRAM Memory to store it. There is a macro defined `BACK_COL_ADR` pointing to the A1 bank of the VRAM VDP2
+    - The second parameter is the actual color, there are some macro `CD_[Color]` defines in SGL. But, 0 or CD_BLACK is black
+- Setup of the **Character patterns** by invoking `slCharNbg1`. The example uses the NBG1 normal scroll
+    - `COL_TYPE_256` refers to 256 colors
+    - `CHAR_SIZE_1x1` refers to the character size
+- Setup of the **Pages** by invoking `slPageNbg1`:
+    - The first parameter `NBG1_CEL_ADR` macro defined is the starting memory address of the character pattern data used on the scroll screen
+    - The second parameter `0` indicates the starting address in color RAM of color data used by cells
+    - The third parameter indicates the type (pattern name) word size used. In this case is configured to `Low-order 12 bit` `PNB_1WORD|CN_12BIT`. It is very important to have the Developer Guide and Reference for understanding what this means (Fig 8-10 Pattern Name Data Concept).
+- Setup of the **Planes** size with the routine `slPlaneNbg1`
+    - `PL_SIZE_1x1` indicates 1 horizontal x 1 vertical
+- Setup of the normal scroll **Map** with the routine `slMapNbg1` which parameters (starting address in VRAM of the pattern name data table) points to the same memory address in VRAM
+- Store of the **Cell** and **Map** in VRAM using some auxiliar routines defined in `scl_func.c`. **Note** that this is not part of SGL, but can be re-used
+    - `Cel2VRAM` is a routine that receives the **Cel** data, the starting address memory for **Cel** data and the character size
+    - `Map2VRAM` is the routine to store the map information
+- Store **Color Palette** in Color RAM using the routine `Pal2CRAM` defined in `scl_func.c` 
+- Register the initial position of the Scroll invoking `slScrPosNbg1`
+- Register the Scrolls NBG0 and NBG1 by invoking `slScrAutoDisp` OR separated every scroll
+- Turn on the drawing again by invoking `slTVOn`
+- The main loop will:
+    - Position in the screen in the new calculated position by invoking `slScrPosNbg1`
+    - Update the X position
+    - Synchronize with event processing unit time `slSynch` as previous examples
 
 
 
--- Projects/S_8_8_1/main.c
+++ Projects/S_8_8_1/main.c
@@ -8,10 +8,10 @@
 // VRAM-A0 (0x25e00000), VRAM-A1 (0x25e20000), VRAM-B0(0x25e40000), VRAM-B1(0x25e60000)
 // Color RAM, CRAM (0x25f00000)
 
-#define		NBG1_CEL_ADR		( VDP2_VRAM_B1 + 0x02000 ) // Starting memory address for cells, remember the beginning for CEL is reserve until 0x25e61FFF
-#define		NBG1_MAP_ADR		( VDP2_VRAM_B1 + 0x12000 ) // Starting memory address for the maps, remember the beginning for MAP is reserve until 0x25e76FFF
-#define		NBG1_COL_ADR		( VDP2_COLRAM + 0x00200 ) // Starting memory address for the color RAM, remeber the beginning for CRAM is reserver until 0x25f0001F
-#define		BACK_COL_ADR		( VDP2_VRAM_A1 + 0x1fffe ) // Background color address will be situated in A1 bank area. 
+#define	NBG1_CEL_ADR	( VDP2_VRAM_B1 + 0x02000 ) // Starting memory address for cells, remember the beginning for CEL is reserve until 0x25e61FFF
+#define	NBG1_MAP_ADR	( VDP2_VRAM_B1 + 0x12000 ) // Starting memory address for the maps, remember the beginning for MAP is reserve until 0x25e76FFF
+#define	NBG1_COL_ADR	( VDP2_COLRAM + 0x00200 ) // Starting memory address for the color RAM, remeber the beginning for CRAM is reserver until 0x25f0001F
+#define	BACK_COL_ADR	( VDP2_VRAM_A1 + 0x1fffe ) // Background color address will be situated in A1 bank area. 
 
 // Note that Sega Saturn has 4 banks of VRAM memory of 1 megabit each one (A0, A1, B1, B2)
 /*
@@ -36,10 +36,10 @@ int main(void) {
 	slPrint("Sample program 8.8.1" , slLocate(9,2));
 
 	slColRAMMode(CRM16_1024); // Set the Color RAM mode to mode 0 (16 bit and 1024 color)
-	slBack1ColSet((void *)BACK_COL_ADR , 0); // Set the background of the screen
+	slBack1ColSet((void *)BACK_COL_ADR , CD_Black); // Set the background of the screen to color black and store it in VDP2 VRAM A1 bank
 
-	// Setup of the scroll settings (Cells, Page, Planes and Map). In htis case NBG1, please refer to Scroll Function Settings in the documentation
-	slCharNbg1(COL_TYPE_256 , CHAR_SIZE_1x1); // Setup character patterns
+	// Setup of the scroll settings (Cells, Page, Planes and Map). In this case NBG1, please refer to Scroll Function Settings in the documentation
+	slCharNbg1(COL_TYPE_256 , CHAR_SIZE_1x1); // Setup character pattern
 	slPageNbg1((void *)NBG1_CEL_ADR , 0 , PNB_1WORD|CN_12BIT); // Setup pages
 	slPlaneNbg1(PL_SIZE_1x1); // Setup up planes
 	slMapNbg1((void *)NBG1_MAP_ADR , (void *)NBG1_MAP_ADR , (void *)NBG1_MAP_ADR , (void *)NBG1_MAP_ADR); // Setup maps
@@ -51,7 +51,7 @@ int main(void) {
 	Pal2CRAM(yama_pal , (void *)NBG1_COL_ADR , 256); // Storing Palette information in VRAM (VDP2)
 
 	// Register the scroll screen
-	slScrPosNbg1(yama_posx , yama_posy); // Positicion the Scroll NBG1 (Normal)
+	slScrPosNbg1(yama_posx , yama_posy); // Position the Scroll NBG1 (Normal)
 	slScrAutoDisp(NBG0ON | NBG1ON); // Registration of the Scrolls NBG0 and NBG1
 
 	slTVOn(); // Start drawing again
